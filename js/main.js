c49495d363e2e1558833e4295d38e1e32b829ff2
Marcelo Berretta <marcelo.berretta@hotmail.com>
2023-03-01 15:30:22 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ document.getElementById('logo2').addEventListener('mouseover',function(){
     mostrar_info('Mi mail','mrcb@live.com.ar');
 })
 document.getElementById('logo3').addEventListener('mouseover',function(){
-    mostrar_info('Mi dirección','Roca 123, CABA');
+    mostrar_info('Mi dirección','El Calafate SC');
 })
 document.getElementById('logo4').addEventListener('mouseover',function(){
-    mostrar_info('Mi teléfono','2966 xxxxxx');
+    mostrar_info('Mi teléfono','2966 640433');
 })
 document.getElementById('logo5').addEventListener('mouseover',function(){
-    mostrar_info('Mi fecha de nacimiento','27 de febrero de 1964');
+    mostrar_info('Mi fecha de nacimiento','27 de febrero');
 })
 
 
